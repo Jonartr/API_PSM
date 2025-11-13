b25b467437ab633002e82f0d5650e70d77c40018
Jonathan Torres <116028104+Jonartr@users.noreply.github.com>
2025-11-12 20:42:53 -0600

    Update AuthController.php

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController
             if ($loginuser) {
                 $this->sendResponse(201, $loginuser);
             } else {
-                $this->sendResponse(404, ["message" => "Datos incorrectos"]);
+                $this->sendResponse(404, ["message" => "Datos incorrectos","data" => $data]);
             }
         }
     }
