392f51a5f8fe3859646b8298387a3f743a23598a
Jonathan Torres <116028104+Jonartr@users.noreply.github.com>
2025-11-13 16:36:47 -0600

    Update PostController.php

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -58,7 +58,7 @@ class PostController
                     }
                 }
 
-                $this->sendResponse(201, ["message" => "Publicacion Correcta", "valor" => $result]);
+                $this->sendResponse(201, ["message" => "Publicacion Correcta", "valor" => $result, "valor 2:" => $result_2]);
             } else {
                 $this->sendResponse(401, ["message" => "Error al crear publicacion"]);
             }
